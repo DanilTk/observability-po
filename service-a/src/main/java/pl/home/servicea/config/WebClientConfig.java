9739b9cd673e6490899dd609748868f0852e0be3
Dan <d.y.tkachenko@gmail.com>
2025-03-02 10:23:48 +0100

    Added zalando / webclient logging

--- service-a/src/main/java/pl/home/servicea/config/WebClientConfig.java
+++ service-a/src/main/java/pl/home/servicea/config/WebClientConfig.java
@@ -1,5 +1,6 @@
 package pl.home.servicea.config;
 
+import lombok.extern.slf4j.Slf4j;
 import org.slf4j.MDC;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
@@ -9,12 +10,15 @@ import org.springframework.web.reactive.function.client.ExchangeFilterFunction;
 import org.springframework.web.reactive.function.client.WebClient;
 import org.springframework.web.reactive.function.client.support.WebClientAdapter;
 import org.springframework.web.service.invoker.HttpServiceProxyFactory;
+import org.zalando.logbook.Logbook;
+import org.zalando.logbook.spring.webflux.LogbookExchangeFilterFunction;
 import pl.home.servicea.client.ServiceBWebClient;
 
 import java.util.UUID;
 
 import static pl.home.servicea.config.CorrelationIdFilter.CORRELATION_ID_HEADER;
 
+@Slf4j
 @Configuration
 public class WebClientConfig {
 
@@ -22,18 +26,19 @@ public class WebClientConfig {
 	private String bServiceUrl;
 
 	@Bean
-	public ServiceBWebClient serviceBWebClient() {
-		WebClient webClient = getWebClient();
+	public ServiceBWebClient serviceBWebClient(Logbook logbook) {
+		WebClient webClient = getWebClient(logbook);
 		HttpServiceProxyFactory factory = HttpServiceProxyFactory.builder()
 			.exchangeAdapter(WebClientAdapter.create(webClient))
 			.build();
 		return factory.createClient(ServiceBWebClient.class);
 	}
 
-	public WebClient getWebClient() {
+	public WebClient getWebClient(Logbook logbook) {
 		return WebClient.builder()
 			.baseUrl(bServiceUrl)
 			.filter(correlationIdFilter())
+			.filter(new LogbookExchangeFilterFunction(logbook))
 			.build();
 	}
 
